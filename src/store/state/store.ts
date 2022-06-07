0c046f5860ee6ee5f07e93ea03b0ca699041df5e
Alex <tendren3310@googlemail.com>
2022-06-07 19:57:44 +0300

    Lesson 11 add dev tools

--- src/store/state/store.ts
+++ src/store/state/store.ts
@@ -1,15 +1,15 @@
-import {combineReducers, legacy_createStore} from 'redux'
+import {combineReducers, legacy_createStore as createStore,} from 'redux'
 import {tasksReducer} from '../tasksReducer';
 import {todolistsReducer} from '../todolistsReducer';
+import {devToolsEnhancer} from '@redux-devtools/extension';
 
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todoLists: todolistsReducer
 })
-export const store = legacy_createStore(rootReducer)
+
+export const store = createStore(rootReducer, devToolsEnhancer())
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-// @ts-ignore
-window.store = store
